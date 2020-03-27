bf0b57697b54401e7135a14fdc957a90986e6820
1901274 <1901274@uad.ac.uk>
2020-03-27 15:00:54 +0000

    Completed Lab 2

--- Week2/CMP105App/Level.cpp
+++ Week2/CMP105App/Level.cpp
@@ -1,4 +1,8 @@
 #include "Level.h"
+int originalx = 0, originaly = 0;
+int finalx = 0, finaly = 0;
+bool gotStart = 0;
+
 
 Level::Level(sf::RenderWindow* hwnd, Input* in)
 {
@@ -6,32 +10,83 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 	input = in;
 
 	// initialise game objects
+	if (!font.loadFromFile("D:/UNI/Sem 2/Games Development/CMP105_W1/Week1/CMP105App/font/arial.ttf"))
+	{
+		std::cout << "Error loading font\n";
+	}
 
+	mousePosText.setFont(font);
+	mousePosText.setCharacterSize(24);
+	mousePosText.setFillColor(sf::Color::Red);
+
+	circle.setRadius(100);
+	circle.setFillColor(sf::Color::Magenta);
+	circle.setOrigin(100, 100);
 }
 
 Level::~Level()
 {
 	input = nullptr;
+
 }
 
 // handle user input
 void Level::handleInput()
 {
+	if (input->isKeyDown(sf::Keyboard::W))
+	{
+		input->setKeyUp(sf::Keyboard::W);
+		std::cout << "W key was pressed\n";
+	}
 
+	if ((input->isKeyDown(sf::Keyboard::K)) & (input->isKeyDown(sf::Keyboard::L)) & (input->isKeyDown(sf::Keyboard::J)))
+	{
+		input->setKeyUp(sf::Keyboard::K);
+		input->setKeyUp(sf::Keyboard::L);
+		input->setKeyUp(sf::Keyboard::J);
+		std::cout << "K, L and J keys were pressed\n";
+	}
 
+	if (input->isKeyDown(sf::Keyboard::Escape))
+	{
+		input->setKeyUp(sf::Keyboard::Escape);
+		window->close();
+	}
+
+	if (input->isMouseLDown() && gotStart == 0)
+	{
+		originalx = input->getMouseX();
+		originaly = input->getMouseY();
+		gotStart = 1;
+
+	}
+	else if (!(input->isMouseLDown()) && gotStart == 1)
+	{
+		finalx = input->getMouseX();	
+		finaly = input->getMouseY();
+		gotStart = 0;
+		std::cout << "The X distance of that drag was " << (finalx - originalx) << ", The Y distance was " << (finaly - originaly);
+	}
+
+	if (input->isMouseRDown())
+	{
+		circle.setPosition(input->getMouseX(), input->getMouseY());
+		input->setMouseRDown(0);
+	}
 }
 
 // Update game objects
 void Level::update()
 {
-
+	mousePosText.setString(" Mouse X: " + std::to_string(input->getMouseX()) + "	Mouse Y: " + std::to_string(input->getMouseY()));
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
-
+	window->draw(mousePosText);
+	window->draw(circle);
 	endDraw();
 }
 
